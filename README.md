9548f9d393472f06ef68180f2777b04ba351f108
Loezer-LAJES <loezer.lucas@gmail.com>
2017-08-24 16:28:33 -0300

    Coloquei algumas coisas no README.md para me orientar melhor

--- README.md
+++ README.md
@@ -3,4 +3,12 @@
 ### Notas:
 - Spouts: componente responsável pela leitura de tuplas de uma fonte externa de dados e pela emissão delas para a topoligia.
 - Bolts: componente responsável pelo processamento dos dados.
-- Testar storm, tentar rodar um exemplo e criar estruturas de dados dele utilizando as instances do Moa.
+- Testar o storm com estruturas do MOA.
+
+
+
+- O calculo da distancia euclideana vai emitir uma tupla (Instance, Double), vou usar a mesma estratégia da variável que usei no flink.
+- O prequential será um spout ou um bolt, ainda não tenho certeza.
+- Os procedimentos do classificador serão bolts, provavelmente terei que usar a variavel para setar eles.
+http://storm.apache.org/releases/1.1.0/Tutorial.html
+
--- src/main/java/Experimenter.java
+++ src/main/java/Experimenter.java
@@ -1,4 +1,18 @@
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.testing.TestWordSpout;
+import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.utils.Utils;
+
+import java.util.Map;
 
 /**
  * Created by loezerl-fworks on 23/08/17.
@@ -9,9 +23,71 @@ import org.apache.storm.topology.TopologyBuilder;
 
 public class Experimenter {
 
-    public static void main(String[] args) throws Exception{
+    public static class ExclamationBolt extends BaseRichBolt {
+        OutputCollector _collector;
 
+        @Override
+        public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+            _collector = collector;
+        }
+
+        @Override
+        public void execute(Tuple tuple) {
+            _collector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
+            _collector.ack(tuple);
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("word"));
+        }
+
+    }
+
+    public static class EuclideanDistanceBolt extends BaseRichBolt{
+        OutputCollector _collector;
+
+        @Override
+        public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+            _collector = collector;
+        }
+
+        @Override
+        public void execute(Tuple tuple) {
+            _collector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
+            _collector.ack(tuple);
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("euclidean_distance"));
+        }
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
         TopologyBuilder builder = new TopologyBuilder();
-        System.out.println("dummy print");
+
+        builder.setSpout("word", new TestWordSpout(), 10);
+        builder.setBolt("exclaim1", new ExclamationBolt(), 3).shuffleGrouping("word");
+        builder.setBolt("exclaim2", new ExclamationBolt(), 2).shuffleGrouping("exclaim1");
+
+        Config conf = new Config();
+        conf.setDebug(false);
+
+        if (args != null && args.length > 0) {
+            conf.setNumWorkers(3);
+
+            StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
+        }
+        else {
+
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("test", conf, builder.createTopology());
+            Utils.sleep(10000);
+            cluster.killTopology("test");
+            cluster.shutdown();
+        }
     }
 }
